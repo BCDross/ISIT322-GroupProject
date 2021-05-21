d34b98ebda854011afade57138d20b0b33e7887d
Travis Simmers <travis.s.simmers@gmail.com>
2021-05-20 21:30:11 -0700

    Navigation drawer created, blank views created for (5) pages, home, bookshelf, search, help, and privacy.
    Navigation drawer added. Fragments (views) added for home, bookshelf, search, help, and privacy pages. New fragments will be added later for: login and profile(maybe?).

---
+++ RookandLochBooks/app/src/main/java/com/hfad/rookandlochbooks/ui/search/SearchViewModel.java
@@ -0,0 +1,19 @@
+package com.hfad.rookandlochbooks.ui.search;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SearchViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public SearchViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is the search page fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
