66f486eb9815b5a75159d6e70592eda4a33cc4bd
drewbaby <andrew.lawson@bellevuecollege.edu>
2021-06-01 13:18:03 -0700

    Added models for Book Information, Imagelinks, Industryidenfifier, readingmodes and panaelizationsummary to the model java package folder.   This will be used when making the external RestAPI call

---
+++ RookandLochBooks/app/src/main/java/com/hfad/rookandlochbooks/data/model/PanelizationSummary.java
@@ -0,0 +1,24 @@
+package com.hfad.rookandlochbooks.data.model;
+
+public class PanelizationSummary {
+
+    private Boolean containsEpubBubbles;
+    private Boolean containsImageBubbles;
+
+    public Boolean getContainsEpubBubbles() {
+        return containsEpubBubbles;
+    }
+
+    public void setContainsEpubBubbles(Boolean containsEpubBubbles) {
+        this.containsEpubBubbles = containsEpubBubbles;
+    }
+
+    public Boolean getContainsImageBubbles() {
+        return containsImageBubbles;
+    }
+
+    public void setContainsImageBubbles(Boolean containsImageBubbles) {
+        this.containsImageBubbles = containsImageBubbles;
+    }
+
+}
